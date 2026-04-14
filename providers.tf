ccc7fb167e0908939bc230a6254e4f6d636cd31f
miarorandriama <miarorandriama@gmail.com>
2026-04-14 23:06:28 +0300

    Refactor AWS provider configuration to enhance readability and maintainability

--- providers.tf
+++ providers.tf
@@ -10,4 +10,14 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
+  access_key = "test"
+  secret_key = "test"
+
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
+
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
 }
