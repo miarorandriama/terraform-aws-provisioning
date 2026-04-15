6060b1d400bf68bca534de72c429997e47f59095
miarorandriama <miarorandriama@gmail.com>
2026-04-15 22:16:27 +0300

    Add missing S3 and IAM endpoints to AWS provider configuration

--- providers.tf
+++ providers.tf
@@ -15,9 +15,13 @@ provider "aws" {
 
   endpoints {
     ec2 = "http://localhost:4566"
+    s3 = "http://localhost:4566"
+    iam = "http://localhost:4566"
   }
 
   skip_credentials_validation = true
   skip_metadata_api_check = true
   skip_requesting_account_id = true
+
+  s3_use_path_style = true
 }
